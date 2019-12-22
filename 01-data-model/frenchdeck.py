fc1b03708ee54a5832525d481e9ef2707f30b98a
leequant761 <leequant761@gmail.com>
2019-12-22 23:26:49 +0900

    studied chapter1

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -1,5 +1,10 @@
 import collections
 
+# 네임드 튜플이란?
+# a = Card(10, 20)
+# a.suit
+# 코드의 가독성을 위해서 튜플을 대신한다. 하지만 딕셔너리를 사용하지 않는 이유는
+# 불변성과 메모리 낭비를 줄이기 위해서이다. 만약에 사전으로 바꾸고 싶다면 a._asdict()
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeck:
@@ -15,3 +20,38 @@ class FrenchDeck:
 
     def __getitem__(self, position):
         return self._cards[position]
+
+
+beer_card = Card('7', 'diamonds')
+beer_card # Card(rank='7', suit='diamonds')
+
+
+# 카드가 몇장 있는 지, __len__ 이라는 특별 메서드를 호출
+deck = FrenchDeck()
+len(deck) # 52 
+
+
+# 인덱스로 카드 읽기, __getitem__ 호출
+deck[0] # Card(rank='2', suit='spades')
+deck[-1] # Card(rank='A', suit='hearts')
+# 이를 활용하여 랜덤하게 하나를 메서드 정의 없이 뽑을 수 있다.
+# __getitem__ 이 정의된 객체는 시퀀스([index]로 접근 가능)라고 불리고 슬라이싱도 지원한다.
+
+
+# PSL의 random.choice()를 활용하면 무작위로 골라낼 수 있다.
+from random import choice
+choice(deck)
+
+
+# 정렬
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank) # index('item')은 'item'의 위치
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+sorted(deck._cards, key=spades_high)
+
+
+# 지금까지 구현한 것으로는 카드 셔플링을 할 수 없다. _cards가 불변객체라서 제거가 안됨
+# 이를 위해선 __setitem__()을 사용 ; 11장
--- 01-data-model/vector2d.py
+++ 01-data-model/vector2d.py
@@ -8,6 +8,7 @@ class Vector:
 
     def __repr__(self):
         return 'Vector(%r, %r)' % (self.x, self.y)
+        # return 'Vector({0}, {1})'.format(self.x, self.y)
 
     def __abs__(self):
         return hypot(self.x, self.y)
@@ -22,3 +23,64 @@ class Vector:
 
     def __mul__(self, scalar):
         return Vector(self.x * scalar, self.y * scalar)
+
+
+# 특수 메서드는 직접 호출하지 않는다(rj).
+
+v1 = Vector(2, 4)
+v2 = Vector(2, 1)
+v1 + v2 # Vector(4, 5)
+
+v = Vector(3, 4)
+
+
+
+
+
+
+
+
+from math import hypot
+
+class Vector:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector({0}, {1})'.format(self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+
+
+# 특수 메서드는 직접 호출하지 않는다(almost).
+
+v1 = Vector(2, 4)
+v2 = Vector(2, 1)
+v1 + v2 # Vector(4, 5)
+
+v = Vector(3, 4)
+
+
+# __str__ vs __repr__
+# 두 메서드를 동시에 정의한다면 __str__은 사용자에게 더 보기 좋은 식으로 표현
+# 만약 하나만 정의해야 한다면 __repr__을 정의하면 된다.
+# __repr__은 대화형 인터프리터에서 보여줄 뿐만 아니라 __str__이 없을 때 대체하기 떄문이다.
+
+
+# 사용자 정의 __bool__
+# 조건문에서 __bool__을 정의하지 않았다면 __len__을 호출하고 이마저도 없다면
+# 그냥 참값으로 본다.
